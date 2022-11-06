bbcc2dd7e0cfd26c858025f1540db9441ea8e2e0
Jules <jdessors@gmail.com>
2022-11-06 20:58:39 +0100

    ✨ - Can now parse brainfuck.

--- main.c
+++ main.c
@@ -1,49 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void manageChar(char c)
+#define PARSED_CODE_SIZE 50
+
+int isPartOfBf(char c)
 {
-  switch (c)
+  char *bfChars = "><+-,.[]";
+  int i = 0;
+
+  while (bfChars[i] != '\0')
   {
-  case '>':
-    break;
-  case '<':
-    break;
-  case '+':
-    break;
-  case '-':
-    break;
-  case '.':
-    break;
-  case ',':
-    break;
-  case '[':
-    break;
-  case ']':
-    break;
-  default:
-    break;
+    if (bfChars[i] == c)
+      return 1;
+    ++i;
   }
+  return 0;
+}
+
+int parse(char **target, int *targetLength, FILE *fp)
+{
+  int i = 0;
+  char readChar;
+
+  do
+  {
+    readChar = fgetc(fp);
+
+    if (isPartOfBf(readChar))
+    {
+      (*target)[i++] = readChar;
+    }
+
+    if (i >= *targetLength)
+    {
+      *targetLength = 2 * (*targetLength);
+      char *tmp = realloc(*target, sizeof(char) * (*targetLength));
+
+      if (tmp == NULL)
+      {
+        printf("Failure: failed to augment allocated memory.\n");
+        free(*target);
+        return EXIT_FAILURE;
+      }
+
+      *target = tmp;
+    }
+  } while (readChar != EOF);
+
+  return EXIT_SUCCESS;
 }
 
 int main(void)
 {
-  char readChar;
-  char *filename = "./tests/hello_world.bf";
+  char *filename = "./tests/test.bf";
   FILE *fp = fopen(filename, "r");
 
   if (fp == NULL)
   {
     printf("Failure: specified file could not be open.\n");
-    return 1;
+    return EXIT_FAILURE;
   }
 
-  do
+  int parsedCodeLength = PARSED_CODE_SIZE;
+  char *parsedCode = malloc(sizeof(char) * parsedCodeLength);
+
+  if (parsedCode == NULL)
   {
-    readChar = fgetc(fp);
-    manageChar(readChar);
+    printf("Failure: failed to allocate memory.\n");
+    return EXIT_FAILURE;
+  }
 
-  } while (readChar != EOF);
-
+  int failedParsing = parse(&parsedCode, &parsedCodeLength, fp);
   fclose(fp);
-  return 0;
+
+  if (failedParsing)
+  {
+    printf("Failure: failed to parse code.\n");
+    return EXIT_FAILURE;
+  }
+
+  printf("; Parsed Code Length : %d\n", parsedCodeLength);
+  printf("; Parsed code : '%s'\n", parsedCode);
+
+  free(parsedCode);
+  return EXIT_SUCCESS;
 }
