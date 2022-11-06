6bb88f5bc76c7a9fb13d753b1718e2bceb4d1591
Jules <jdessors@gmail.com>
2022-11-06 15:28:06 +0100

    ✨ - Now reads a file, yay.

--- main.c
+++ main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 
+void manageChar(char c)
+{
+  switch (c)
+  {
+  case '>':
+    break;
+  case '<':
+    break;
+  case '+':
+    break;
+  case '-':
+    break;
+  case '.':
+    break;
+  case ',':
+    break;
+  case '[':
+    break;
+  case ']':
+    break;
+  default:
+    break;
+  }
+}
+
 int main(void)
 {
+  char readChar;
+  char *filename = "./tests/hello_world.bf";
+  FILE *fp = fopen(filename, "r");
 
+  if (fp == NULL)
+  {
+    printf("Failure: specified file could not be open.\n");
+    return 1;
+  }
+
+  do
+  {
+    readChar = fgetc(fp);
+    manageChar(readChar);
+
+  } while (readChar != EOF);
+
+  fclose(fp);
   return 0;
 }
